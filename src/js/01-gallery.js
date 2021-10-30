39d55c6de67cc117f6df246e4ba14ff57c40caf9
Daemon-L <lit.dmitriy.ua@gmail.com>
2021-10-30 17:13:23 +0300

    Задание 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const createitem = galleryItems
+  .map(({ preview, original, description }) =>
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`,
+  )
+    .join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', createitem);
+
+// console.log(galleryItems);
+
+
