31a15e9ac5cc06682135258a34a2df6a50e0da59
Daemon-L <lit.dmitriy.ua@gmail.com>
2021-10-30 18:41:57 +0300

    Выполняет задание 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -11,25 +11,18 @@ const galleryContainer = document.querySelector('.gallery');
 const createitem = galleryItems
   .map(({ preview, original, description }) =>
       
-    `<li>
-        <a class="gallery__item" href="${original}"
-            style='display: block; height: 240px'>
-          <img class="gallery__image" src="${preview}" alt="${description}" />
-        </a>
-    </li>`,
+    `<a class="gallery__item" href="${original}"
+      style=' height: 240px'>
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`,
   )
     .join('');
-
 galleryContainer.insertAdjacentHTML('beforeend', createitem);
 
-
-// 2
 const lightbox = new SimpleLightbox('.gallery a', {
     captionsData: 'alt',
     captionDelay: `250`,
-    
 });
-
 // console.log(galleryItems);
 
 
